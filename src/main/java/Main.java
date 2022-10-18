bfa8cf8c2eaeada843546e5b89622167d5a6c84e
wNM <aleks_rus2004@mail.ru>
2022-10-18 18:04:22 +0300

    Сделано

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,5 @@
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import java.io.IOException;
-import java.util.Properties;
-
 public class Main {
-    public static void main(String[] args) throws MessagingException, IOException {
-        Properties p = new Properties();
-        p.load(Main.class.getClassLoader().getResourceAsStream("mail.properties"));
+    public static void main(String[] args) {
 
-        Session ms = Session.getDefaultInstance(p);
-        MimeMessage m = new MimeMessage(ms);
-        m.setFrom(new InternetAddress());
-        m.addRecipients(Message.RecipientType.TO, String.valueOf(new InternetAddress("")));
-        m.setSubject("123");
-        m.setText("321");
-
-        Transport t = ms.getTransport();
-        t.connect( "", "");
-        t.sendMessage(m, m.getAllRecipients());
-        t.close();
     }
 }
